a9d4121e538269df95f74247a688a58b345a22e2
zbylon80 <zbylon80@users.noreply.github.com>
2025-09-13 17:18:51 +0200

    fixes for google search console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com', // TODO: podmień na swoją domenę po podpięciu
-  integrations: [tailwind({ applyBaseStyles: true })],
+  integrations: [tailwind({ applyBaseStyles: true }), sitemap()],
   output: 'static',
   // Podgląd w LAN podczas developmentu/preview
   server: {
@@ -15,5 +16,6 @@ export default defineConfig({
   alias: {
     '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
     '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-  }
+  },
+  site: 'https://trollingstones.pl'
 });
