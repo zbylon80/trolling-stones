fb9c43e1570446addcaa830b13f1f4faf7f16249
zbylon80 <34059375+zbylon80@users.noreply.github.com>
2025-10-06 23:02:09 +0200

    Revert "admin panel draft for gigs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: true }), sitemap()],
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  output: 'static',
   // LAN preview during development/preview
   server: {
     host: true,
